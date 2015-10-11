4672cd1a7b385e0247c23ba0385fc7f47f30685a
Bobby Bouwmann <bobbybouwmann@gmail.com>
2015-10-11 14:58:23 +0200

    Add dutch translations

---
+++ resources/lang/nl/reset.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'error' => 'Het is niet mogelijk om uw wachtwoord te resetten, de reset token is niet geldig.',
+
+	'success' => 'Uw wachtwoordt is succesvol gereset. U kunt nu inloggen.',
+
+);
