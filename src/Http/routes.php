72a8e9583665a517fa1fcbb896a44b3abd34e5db
Andrea Marco Sartori <22cerbero22@gmail.com>
2015-10-11 21:29:05 +1100

    Use the controller specified in configuration

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -7,22 +7,22 @@ Route::group(['prefix' => config('_auth.routes_prefix')], function()
 {
 	Route::group(['middleware' => ['guest', 'honeypot']], function()
 	{
-		Route::get(config('_auth.login.route'), ['as' => 'login.index', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@showLogin']);
-		Route::post(config('_auth.login.route'), ['as' => 'login.store', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@login']);
+		Route::get(config('_auth.login.route'), ['as' => 'login.index', 'uses' => config('_auth.controller') . '@showLogin']);
+		Route::post(config('_auth.login.route'), ['as' => 'login.store', 'uses' => config('_auth.controller') . '@login']);
 
-		Route::get(config('_auth.register.route'), ['as' => 'register.index', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@showRegister']);
-		Route::post(config('_auth.register.route'), ['as' => 'register.store', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@register']);
+		Route::get(config('_auth.register.route'), ['as' => 'register.index', 'uses' => config('_auth.controller') . '@showRegister']);
+		Route::post(config('_auth.register.route'), ['as' => 'register.store', 'uses' => config('_auth.controller') . '@register']);
 
-		Route::get(config('_auth.recover.route'), ['as' => 'recover.index', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@showRecover']);
-		Route::post(config('_auth.recover.route'), ['as' => 'recover.store', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@recover']);
+		Route::get(config('_auth.recover.route'), ['as' => 'recover.index', 'uses' => config('_auth.controller') . '@showRecover']);
+		Route::post(config('_auth.recover.route'), ['as' => 'recover.store', 'uses' => config('_auth.controller') . '@recover']);
 
-		Route::get(config('_auth.reset.route') . '/{token}', ['as' => 'reset.index', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@showReset']);
-		Route::post(config('_auth.reset.route') . '/{token}', ['as' => 'reset.store', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@reset']);
+		Route::get(config('_auth.reset.route') . '/{token}', ['as' => 'reset.index', 'uses' => config('_auth.controller') . '@showReset']);
+		Route::post(config('_auth.reset.route') . '/{token}', ['as' => 'reset.store', 'uses' => config('_auth.controller') . '@reset']);
 	});
 
 
 	Route::group(['middleware' => 'auth'], function()
 	{
-		Route::get(config('_auth.logout.route'), ['as' => 'logout', 'uses' => 'Cerbero\Auth\Http\Controllers\AuthController@logout']);
+		Route::get(config('_auth.logout.route'), ['as' => 'logout', 'uses' => config('_auth.controller') . '@logout']);
 	});
 });
