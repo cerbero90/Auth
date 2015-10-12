94a8f0fc3137d66f55585afcf7cee66f0d9475a8
Christian Giupponi <chri788@gmail.com>
2015-10-12 15:06:45 +0200

    Italian language

---
+++ resources/lang/it/recover.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'success' => 'Ti è stata inviata una email per il reset della tua password, controlla la tua casella email.',
+
+	'email_subject' => 'Reset password',
+
+);
